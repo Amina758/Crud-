4881dd2601637d6db98ab0baf3d480789eb82902
Amina Binyameen <aminabinyameen51@gmail.com>
2024-12-08 20:27:37 +0500

    commit

---
+++ crudproject/src/Employe data.js
@@ -0,0 +1,51 @@
+export const Data=[
+    {
+    id:1,
+    firstName:"Amina",
+    lastName:"Binyameen",
+    age:"18",
+    
+    
+    },
+    
+    {
+        id:2,
+        firstName:"Fatima",
+        lastName:"Binyameen",
+        age:"12",
+        
+        
+        },
+        {
+            id:3,
+            firstName:"Esha",
+            lastName:"Razaq",
+            age:"20",
+            
+            
+            },
+            
+            {
+                id:4,
+                firstName:"Ayesha",
+                lastName:"Rahoom",
+                age:"18",
+                
+                
+                },  
+                
+                    
+        
+                {
+                    id:5,
+                    firstName:"Taiba",
+                    lastName:"Liaqat",
+                    age:"16",
+                    
+                    
+                    }
+
+
+
+
+    ]
